81086de756e0dded526ca627063598b6a861431f
Jakob Riedle <jakob.riedle@gmx.de>
2023-07-26 16:05:45 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+from tiny_parser import Rule, TokenType, AST
+import tiny_parser
+
+# C++ Math language
+class Statement(AST): pass
+class Programm(AST): pass
+class BinaryOperation(AST): pass
+class UnaryOperation(AST): pass
+class Literal(AST): pass
+class Identifier(AST): pass
+class CppToken(TokenType):
+    DOUBLE_PLUS             = r"^\+\+"
+    DOUBLE_MINUS            = r"^--"
+    PLUS                    = r"^\+"
+    MINUS                   = r"^-"
+    TIMES                   = r"^\*"
+    DIVIDES                 = r"^/"
+    POWER                   = r"^\^"
+    LESS_EQUAL              = r"^<="
+    GREATER_EQUAL           = r"^>="
+    LESS                    = r"^<"
+    GREATER                 = r"^>"
+    LEFT_PARENTHESIS        = r"^\("
+    RIGHT_PARENTHESIS       = r"^\)"
+    LEFT_SQUARE_BRACKET     = r"^\["
+    RIGHT_SQUARE_BRACKET    = r"^\]"
+    SEMICOLON               = r"^;"
+    COLON                   = r"^:"
+    COMMA                   = r"^,"
+    DOT                     = r"^\."
+    IDENTIFIER              = r"^[a-zA-Z_][a-zA-Z0-9_]*\b"
+    LITERAL                 = r"^[1-9][0-9]*(\.[0-9]*)?\b|\.[0-9]+\b|0\b"
+CppGrammar = {
+    "0.1": Rule( BinaryOperation , ("1", "operands") , ([CppToken.PLUS, CppToken.MINUS], "type") , ("0", "operands") ),
+    "0.2": Rule( None , "1" ),
+    "1.1": Rule( BinaryOperation , ("2", "operands") , ([CppToken.TIMES, CppToken.DIVIDES], "type") , ("1", "operands") ),
+    "1.2": Rule( None , "2" ),
+    "2.1": Rule( UnaryOperation , ([CppToken.MINUS, CppToken.PLUS], "type") , "2" ),
+    "2.2": Rule( None , "3" ),
+    "3.1": Rule( None , CppToken.LEFT_PARENTHESIS , "0" , CppToken.RIGHT_PARENTHESIS ),
+    "3.2": Rule( None , "4" ),
+    "4.1": Rule( Literal , (CppToken.LITERAL, "value") ),
+    "4.2": Rule( Identifier , (CppToken.IDENTIFIER, "name") ),
+}
+
+tiny_parser.print_ast( tiny_parser.parse( CppToken , CppGrammar, "pi*4/3+(-5+foo)" , "0") )
