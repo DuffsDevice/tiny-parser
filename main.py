8a4a545021befc2750517acfb4e0d739914c356d
Jakob Riedle <jakob.riedle@gmx.de>
2023-07-26 16:53:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,17 +31,17 @@ class CppToken(TokenType):
     IDENTIFIER              = r"^[a-zA-Z_][a-zA-Z0-9_]*\b"
     LITERAL                 = r"^[1-9][0-9]*(\.[0-9]*)?\b|\.[0-9]+\b|0\b"
 CppGrammar = {
-    "0.1": Rule( BinaryOperation , ("1", "operands") , ([CppToken.PLUS, CppToken.MINUS], "type") , ("0.2", "operands") ),
-    "0.2.1": Rule( None , ("1", "operands") , ([CppToken.PLUS, CppToken.MINUS], "type") , ("0.2", "operands") ),
-    "0.2.2": Rule( None , "1" ),
-    "1.1": Rule( BinaryOperation , ("2", "operands") , ([CppToken.TIMES, CppToken.DIVIDES], "type") , ("1", "operands") ),
+    "0.1": Rule( BinaryOperation , ("1", "left") , ([CppToken.PLUS, CppToken.MINUS], "type") , ("0", "right") ),
+    "0.2": Rule( None , "1" ),
+    "1.1": Rule( BinaryOperation , ("2", "left") , ([CppToken.TIMES, CppToken.DIVIDES], "type") , ("1", "right") ),
     "1.2": Rule( None , "2" ),
-    "2.1": Rule( UnaryOperation , ([CppToken.MINUS, CppToken.PLUS], "type") , ("2", "operand") ),
-    "2.2": Rule( None , "3" ),
+    "2.1": Rule( UnaryOperation , ([CppToken.MINUS, CppToken.PLUS, CppToken.DOUBLE_PLUS, CppToken.DOUBLE_MINUS], "type") , ("2", "operand") ),
+    "2.2": Rule( UnaryOperation , ("3", "operand"), ([CppToken.DOUBLE_PLUS, CppToken.DOUBLE_MINUS], "type") ),
+    "2.3": Rule( None , "3" ),
     "3.1": Rule( None , CppToken.LEFT_PARENTHESIS , "0" , CppToken.RIGHT_PARENTHESIS ),
     "3.2": Rule( None , "4" ),
     "4.1": Rule( Literal , (CppToken.LITERAL, "value") ),
     "4.2": Rule( Identifier , (CppToken.IDENTIFIER, "name") ),
 }
 
-tiny_parser.print_ast( tiny_parser.parse( CppToken , CppGrammar, "1+2- -3" , "0") )
+tiny_parser.print_ast( tiny_parser.parse( CppToken , CppGrammar, "1+2+3" , "0") )
