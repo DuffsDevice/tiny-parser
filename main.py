4b1d44b89e7e450109ec954e3a601db3c92e5f16
Jakob Riedle <jakob.riedle@gmx.de>
2023-07-31 09:42:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,27 +48,27 @@ class CppToken(TokenType):
     LITERAL                 = r"^[1-9][0-9]*(\.[0-9]*)?\b|\.[0-9]+\b|0\b"
 CppGrammar = {
     # Statement Blocks
-    "0.1": Rule( Program , ("10", "statements") , ("1", "statements") ),
-    "0.2": Rule( Program , ("10", "statements") ),
-    "1.1": Rule( None , ("10", "statements") , ("1", "statements") ),
-    "1.2": Rule( None , ("10", "statements") ),
+    "0.1": Rule( Program , ("10.", "statements") , ("1.", "statements") ),
+    "0.2": Rule( Program , ("10.", "statements") ),
+    "1.1": Rule( None , ("10.", "statements") , ("1.", "statements") ),
+    "1.2": Rule( None , ("10.", "statements") ),
 
     # Statement
     "10.1": Rule( Statement , CppToken.SEMICOLON ),
     "10.2": Rule( StatementBlock , CppToken.LEFT_CURLY_BRACKET , CppToken.RIGHT_CURLY_BRACKET ),
-    "10.2": Rule( StatementBlock , CppToken.LEFT_CURLY_BRACKET , ("1.1", "statements") , CppToken.RIGHT_CURLY_BRACKET ),
-    "10.3": Rule( ExpressionStatement , ("20", "expression") , CppToken.SEMICOLON ),
+    "10.2": Rule( StatementBlock , CppToken.LEFT_CURLY_BRACKET , ("1.1.", "statements") , CppToken.RIGHT_CURLY_BRACKET ),
+    "10.3": Rule( ExpressionStatement , ("20.", "expression") , CppToken.SEMICOLON ),
 
     # Expression
-    "20.1": Rule( BinaryOperation , ("21", "left") , ([CppToken.PLUS, CppToken.MINUS], "type") , ("20", "right") ),
-    "20.2": Rule( None , "21" ),
-    "21.1": Rule( BinaryOperation , ("22", "left") , ([CppToken.TIMES, CppToken.DIVIDES], "type") , ("21", "right") ),
-    "21.2": Rule( None , "22" ),
-    "22.1": Rule( UnaryOperation , ([CppToken.MINUS, CppToken.PLUS, CppToken.DOUBLE_PLUS, CppToken.DOUBLE_MINUS], "type") , ("22", "operand") ),
-    "22.2": Rule( UnaryOperation , ("23", "operand"), ([CppToken.DOUBLE_PLUS, CppToken.DOUBLE_MINUS], "type") ),
-    "22.3": Rule( None , "23" ),
-    "23.1": Rule( None , CppToken.LEFT_PARENTHESIS , "20" , CppToken.RIGHT_PARENTHESIS ),
-    "23.2": Rule( None , "24" ),
+    "20.1": Rule( BinaryOperation , ("21.", "left") , ([CppToken.PLUS, CppToken.MINUS], "type") , ("20.", "right") ),
+    "20.2": Rule( None , "21." ),
+    "21.1": Rule( BinaryOperation , ("22.", "left") , ([CppToken.TIMES, CppToken.DIVIDES], "type") , ("21.", "right") ),
+    "21.2": Rule( None , "22." ),
+    "22.1": Rule( UnaryOperation , ([CppToken.MINUS, CppToken.PLUS, CppToken.DOUBLE_PLUS, CppToken.DOUBLE_MINUS], "type") , ("22.", "operand") ),
+    "22.2": Rule( UnaryOperation , ("23.", "operand"), ([CppToken.DOUBLE_PLUS, CppToken.DOUBLE_MINUS], "type") ),
+    "22.3": Rule( None , "23." ),
+    "23.1": Rule( None , CppToken.LEFT_PARENTHESIS , "20." , CppToken.RIGHT_PARENTHESIS ),
+    "23.2": Rule( None , "24." ),
     "24.1": Rule( Literal , (CppToken.LITERAL, "value") ),
     "24.2": Rule( Identifier , (CppToken.IDENTIFIER, "name") ),
 }
