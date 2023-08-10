8149ee3d898c9026e2f307024d26fc63a3bc6f32
Jakob Riedle <jakob.riedle@partner.bmw.de>
2023-08-10 16:23:24 +0200

    Minor adjustments

--- tinyparser/language/json.py
+++ tinyparser/language/json.py
@@ -1,18 +1,18 @@
-from .. import Token, Language
+from .. import Rule, Token, Language
 
 json = Language({
     "0.1": (eval, (Token.NUMBER, (None, "value"))),
     "0.2": (None, (Token.STRING, (None, "value"))),
-    "0.3": ("#", Token.LEFT_CURLY_BRACKET, ("1.", "#"), Token.RIGHT_CURLY_BRACKET),
-    "0.4": ("#", Token.LEFT_SQUARE_BRACKET, ("3.", "#"), Token.RIGHT_SQUARE_BRACKET),
+    "0.3": ("#", Token.LEFT_SQUARE_BRACKET, ("1.", "#"), Token.RIGHT_SQUARE_BRACKET),
+    "0.4": ("#", Token.LEFT_CURLY_BRACKET, ("2.", "#"), Token.RIGHT_CURLY_BRACKET),
 
-    "1.0": ({}, ("2.", ""), Token.COMMA, ("1.", "")),
-    "1.1": ({}, ("2.", "")),
-    "1.2": {},
+    "1.1.1": ([], "0.", (Token.COMMA, []), "1.1."),
+    "1.1.2": ([], "0."),
+    "1.2": [],
 
-    "2.0": ({}, (Token.STRING, (None, "value")), Token.COLON, ("0.", (0, "value"))),
+    "2.1.1": ({}, ("3.", ""), Token.COMMA, ("2.1.", "")),
+    "2.1.2": ({}, ("3.", "")),
+    "2.2": {},
 
-    "3.0": ([], "0.", (Token.COMMA, []), "3."),
-    "3.1": ([], "0."),
-    "3.2": [],
+    "3.0": ({}, (Token.STRING, (None, "value")), Token.COLON, ("0.", (0, "value"))),
 })
